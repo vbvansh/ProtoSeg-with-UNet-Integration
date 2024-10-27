092cbab521bd4f80210b5fcd03bc46dd758d7c0d
vb2910 <vb291024@gmail.com>
2024-10-27 10:57:21 +0530

    script files

--- Pytorch_UNet_master/scripts/download_data.sh
+++ Pytorch_UNet_master/scripts/download_data.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
+# Check if Kaggle API credentials are available, and prompt for them if not
 if [[ ! -f ~/.kaggle/kaggle.json ]]; then
-  echo -n "Kaggle username: "
-  read USERNAME
+  read -p "Kaggle username: " USERNAME
+  read -s -p "Kaggle API key: " APIKEY
   echo
-  echo -n "Kaggle API key: "
-  read APIKEY
 
   mkdir -p ~/.kaggle
   echo "{\"username\":\"$USERNAME\",\"key\":\"$APIKEY\"}" > ~/.kaggle/kaggle.json
   chmod 600 ~/.kaggle/kaggle.json
 fi
 
-pip install kaggle --upgrade
+# Upgrade kaggle package
+pip install --upgrade kaggle
 
-kaggle competitions download -c carvana-image-masking-challenge -f train_hq.zip
-unzip train_hq.zip
-mv train_hq/* data/imgs/
-rm -d train_hq
-rm train_hq.zip
+# Download and organize the images
+kaggle competitions download -c carvana-image-masking-challenge -f train_hq.zip && unzip -q train_hq.zip -d data/imgs/ && rm train_hq.zip
 
-kaggle competitions download -c carvana-image-masking-challenge -f train_masks.zip
-unzip train_masks.zip
-mv train_masks/* data/masks/
-rm -d train_masks
-rm train_masks.zip
+# Download and organize the masks
+kaggle competitions download -c carvana-image-masking-challenge -f train_masks.zip && unzip -q train_masks.zip -d data/masks/ && rm train_masks.zip
